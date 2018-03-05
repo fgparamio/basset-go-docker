2bbcf55cd2ed730873fe8347d3421b3146f11e7c
fernando <fgparamio@yahoo.es>
2018-03-05 18:13:12 +0000

    First Implementation from Basset go microservice

---
+++ serve.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
+)
+
+// Page is Root element
+type Page struct {
+	Page       int `json:"page"`
+	PerPage    int `json:"per_page"`
+	Total      int `json:"total"`
+	TotalPages int `json:"total_pages"`
+	Data       []User
+}
+
+// PageDTO is root element of DTO
+type PageDTO struct {
+	Page       int       `json:"page"`
+	PerPage    int       `json:"perPage"`
+	Total      int       `json:"total"`
+	TotalPages int       `json:"totalPages"`
+	Users      []UserDTO `json:"users"`
+}
+
+// User is a list Data
+type User struct {
+	ID        int
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Avatar    string
+}
+
+// UserDTO main model
+type UserDTO struct {
+	ID     int
+	Name   string `json:"name"`
+	Avatar string `json:"avatar"`
+}
+
+// ToDTO Convert Page to PageDTO
+func ToDTO(page Page) PageDTO {
+	usersDto := make([]UserDTO, 0)
+	for _, user := range page.Data {
+		usersDto = append(usersDto, UserDTO{ID: user.ID, Name: user.FirstName + " " + user.LastName, Avatar: user.Avatar})
+	}
+	return PageDTO{Page: page.Page, PerPage: page.PerPage, Total: page.Total, TotalPages: page.TotalPages, Users: usersDto}
+}
+
+// GetUsers from Chino Urls
+func GetUsers(w http.ResponseWriter, r *http.Request) {
+	ch := make(chan Page)
+	page := r.URL.Query().Get("page")
+	go makeRequest("https://reqres.in/api/users?page="+page, ch)
+	json.NewEncoder(w).Encode(ToDTO(<-ch))
+
+}
+
+func makeRequest(url string, ch chan<- Page) {
+	response, _ := http.Get(url)
+	responseData, parseErr := ioutil.ReadAll(response.Body)
+	if parseErr != nil {
+		log.Fatal(parseErr)
+	}
+	var page Page
+	json.Unmarshal([]byte(responseData), &page)
+	ch <- page
+}
+
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/users", GetUsers).Methods("GET")
+	corsObj := handlers.AllowedOrigins([]string{"*"})
+	log.Fatal(http.ListenAndServe(":4000", handlers.CORS(corsObj)(router)))
+}
